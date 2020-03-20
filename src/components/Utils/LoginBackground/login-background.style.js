6d54e2c46315198ff5cb59f44ce6cd6160985531
IsmaCadenas <isma10cadenas@hotmail.com>
2020-03-20 20:28:02 +0100

    Little change in login interface

--- src/components/Utils/LoginBackground/login-background.style.js
+++ src/components/Utils/LoginBackground/login-background.style.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const Image = styled.div`
-  background-image: url(${props => props.image});
+  @media screen and (min-width: 992px) {
+    background-image: url(${props => props.image});
+  }
+  @media screen and (max-width: 992px) {
+    background-color: #a4d1d1;
+  }
   background-repeat: no-repeat;
   background-size: cover;
   height: 100%;
