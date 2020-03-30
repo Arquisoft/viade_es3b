4cd5c48746889abbcfc4399b2a17892196b2e6e4
IsmaCadenas <isma10cadenas@hotmail.com>
2020-03-30 13:11:31 +0200

    Uploader internationalized

--- src/containers/Uploader/uploader.component.js
+++ src/containers/Uploader/uploader.component.js
@@ -1,49 +1,46 @@
 import React from "react";
 import { UploaderWrapper, UploaderCard, FileButton, ChooseButton, UploadButton } from './uploader.style';
+import { useTranslation } from 'react-i18next';
 
-class Uploader extends React.Component {
-	
-
-   async run() {
-	   
-	const FC = require('solid-file-client');
-	const auth = require('solid-auth-client');
-		
-	const filesInput = document.getElementById('file-browser-input');
-	const files = filesInput.files;
-	
-	//const containerInput = document.getElementById('container');
-	
-	for (let i = 0; i < files.length; i++) {
-        
-		let reader = new FileReader();
-		let file = files[i];
-		
-		reader.readAsText(file);
-		reader.onload = () => {
+const Uploader = () => {
+	const run = async () => {
+		const FC = require('solid-file-client');
+		const auth = require('solid-auth-client');
 			
-			let fileContent = reader.result;
-			auth.trackSession(session => {
-			if (session) {			
+		const filesInput = document.getElementById('file-browser-input');
+		const files = filesInput.files;
+		
+		//const containerInput = document.getElementById('container');
+		
+		for (let i = 0; i < files.length; i++) {
 			
-				const fc = new FC(auth);	
-				let webId = session.webId;
-				let url = webId.split("/profile/card#me")[0];
-				url = url.concat("/public/myRoutes/").concat(file.name);
+			let reader = new FileReader();
+			let file = files[i];
+			
+			reader.readAsText(file);
+			reader.onload = () => {
 				
-				fc.createFile(url, fileContent);
-				}
-			});
-		};
-	}
-  }
-
-  render() {
-	  
-    return (
-		<UploaderWrapper>
+				let fileContent = reader.result;
+				auth.trackSession(session => {
+				if (session) {			
+				
+					const fc = new FC(auth);	
+					let webId = session.webId;
+					let url = webId.split("/profile/card#me")[0];
+					url = url.concat("/public/myRoutes/").concat(file.name);
+					
+					fc.createFile(url, fileContent);
+					}
+				});
+			};
+		}
+	  };
+	
+  const { t } = useTranslation();
+  return (
+    <UploaderWrapper>
 			<UploaderCard className="card">
-					<h2>Sube aquí los archivos multimedia de tus rutas</h2>
+					<h2>{t('uploader.explanation')}</h2>
 					<FileButton>
 					<div className="inner-container"
 						style= {{display: "flex", flexDirection: "column"}}>
@@ -58,17 +55,15 @@ class Uploader extends React.Component {
 							</div>
 						</ChooseButton>
 						<UploadButton>
-							<button onClick = { () => this.run() } >
-								Subir
+							<button onClick = { run } >
+							{t('uploader.upload')}
 							</button>
 						</UploadButton>
 					</div>
 					</FileButton>
 			</UploaderCard>
 	  	</UploaderWrapper>
-      
-    );
-  }
-}
+  );
+};
 
 export default Uploader;
