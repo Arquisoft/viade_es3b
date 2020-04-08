001bb101a0c8dd7ed4fca9f42a695dbef904a418
ALfonso Lozana <uo264354@uniovi.es>
2020-04-08 17:18:36 +0200

    Bajando rutas de la carpeta estandar de viade

--- src/containers/Map/map.container.js
+++ src/containers/Map/map.container.js
@@ -15,7 +15,7 @@ const LoadRoute = () => {
 
     useEffect(() => {
         if (user !== undefined) {
-            const url = user.split("profile/card#me")[0] + "public/myRoutes";
+            const url = user.split("profile/card#me")[0] + "viade";
             console.log(url);
             //const url = "https://uo264354.solid.community/public/myRoutes";
             //console.log(url);
@@ -27,19 +27,41 @@ const LoadRoute = () => {
 }
 
 async function loadRoutes(url) {
-    let folder = await fileClien.readFolder(url);
+    //let folder = await fileClien.readFolder(url);
+    let routes = await fileClien.readFolder(url + "/routes");
+    //let resources = await fileClien.readFolder(url + "/resources");
     let i;
     let count= 0;
     let rutasJson = [];
-    if(folder.files.length == 0){
+   
+    if(routes.files.length == 0){
         let rutas = new Rutas(rutasJson);
                     ReactDOM.render(<MapaComponent { ... {rutas}}></MapaComponent>, document.getElementById('mapComponent'));
     }
-    for (i = 0; i < folder.files.length; i++) {
-        if (folder.files[i].name.includes('.png')) {
+
+    for (i = 0; i < routes.files.length; i++) {
+        if (routes.files[i].name.includes('.json') || routes.files[i].name.includes('.jsonld')) {
+            fileClien.readFile(url + "/routes/" + routes.files[i].name).then((file) => {
+                rutasJson.push(JSON.parse(file));
+                count += 1;
+                document.getElementById('porcentaje').textContent = "Porcentaje: " + Math.trunc((count) / routes.files.length * 100)  + " %";
+                if(Math.trunc((count) / routes.files.length * 100) === 100) {
+                    let rutas = new Rutas(rutasJson);
+                    ReactDOM.render(<MapaComponent { ... {rutas}}></MapaComponent>, document.getElementById('mapComponent'));
+                }
+            }
+            );
+        } else{
+            document.getElementById('porcentaje').textContent = "Porcentaje: " + Math.trunc((count) / routes.files.length * 100) + " %";
+            count += 1;
+        }
+        
+    }
+    /*for (i = 0; i < folder.files.length; i++) {
+        if (folder.files[i].name.includes('.png') || folder.files[i].name.includes('.jpg')) {
             count += 1;
             document.getElementById('porcentaje').textContent = "Porcentaje: " + Math.trunc((count) / folder.files.length * 100)  + " %";
-        } else if (folder.files[i].name.includes('.json')) {
+        } else if (folder.files[i].name.includes('.jsonld') || folder.files[i].name.includes('.jsonld')) {
             fileClien.readFile(url + "/" + folder.files[i].name).then((file) => {
                 rutasJson.push(JSON.parse(file));
                 count += 1;
@@ -55,7 +77,7 @@ async function loadRoutes(url) {
             count += 1;
         }
         
-    }
+    }*/
 }
 
 class Map extends React.Component{
