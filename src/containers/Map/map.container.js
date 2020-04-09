4fb085baf12918b8e2b2ab7b442ea1416df4bf2d
ALfonso Lozana <uo264354@uniovi.es>
2020-04-09 17:39:31 +0200

    Cambiando diseño del pantalla cargando

--- src/containers/Map/map.container.js
+++ src/containers/Map/map.container.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useWebId } from '@solid/react';
 import ReactDOM from 'react-dom';
 
@@ -6,6 +6,11 @@ import * as solidAuth from 'solid-auth-client';
 import fileClient from 'solid-file-client';
 import MapaComponent from './map.component';
 import Rutas from '../../components/Ruta/rutas';
+
+import {
+    H2Format,
+    InformationSection,
+} from './map.style';
 
 
 const fileClien = new fileClient(solidAuth, { enableLogging: true });
@@ -22,68 +27,61 @@ const LoadRoute = () => {
         }
     }, [user]);
 
-    return (<div id = "mapComponent"><h2 id="porcentaje">Porcentaje: 0 %</h2></div>)
+    return (<InformationSection id="mapComponent"><H2Format id="porcentaje">Cargando: 0 %</H2Format></InformationSection>)
 }
 
 async function loadRoutes(url) {
     let routes = await fileClien.readFolder(url + "/routes");
     let i;
-    
+
     let rutasJson = [];
-   
-    if(routes.files.length === 0){
+
+    if (routes.files.length === 0) {
         let rutas = new Rutas(rutasJson);
         try {
-            ReactDOM.render(<MapaComponent { ... {rutas}}></MapaComponent>, document.getElementById('mapComponent'));
-          }
-          catch(error) {
-              return;
-          }
-          
-                   
+            ReactDOM.render(<MapaComponent {... { rutas }}></MapaComponent>, document.getElementById('mapComponent'));
+        }
+        catch (error) {
+            return;
+        }
+
+
     }
 
     for (i = 0; i < routes.files.length; i++) {
-        var count= 0;
+        var count = 0;
         if (routes.files[i].name.includes('.json') || routes.files[i].name.includes('.jsonld')) {
             console.log(count);
             // eslint-disable-next-line
-            fileClien.readFile(url + "/routes/" + routes.files[i].name).then((file) => { 
+            fileClien.readFile(url + "/routes/" + routes.files[i].name).then((file) => {
                 rutasJson.push(JSON.parse(file));
                 count += 1;
                 try {
-                    document.getElementById('porcentaje').textContent = "Porcentaje: " + Math.trunc((count) / routes.files.length * 100)  + " %";
-                  }
-                  catch(error) {
+                    document.getElementById('porcentaje').textContent = "Cargando: " + Math.trunc((count) / routes.files.length * 100) + " %";
+                }
+                catch (error) {
                     return;
-                  }
-                if(Math.trunc((count) / routes.files.length * 100) === 100) {
+                }
+                if (Math.trunc((count) / routes.files.length * 100) === 100) {
                     let rutas = new Rutas(rutasJson);
-                    try {
-                        ReactDOM.render(<MapaComponent { ... {rutas}}></MapaComponent>, document.getElementById('mapComponent'));
-                      }
-                      catch(error) {
-                        return;
-                      }
+                    setTimeout(() => ReactDOM.render(<MapaComponent {... { rutas }}></MapaComponent>, document.getElementById('mapComponent')), 100);
                 }
             }
             );
-        } else{
+        } else {
             try {
-                document.getElementById('porcentaje').textContent = "Porcentaje: " + Math.trunc((count) / routes.files.length * 100) + " %";
-              }
-              catch(error) {
+                document.getElementById('porcentaje').textContent = "Cargando: " + Math.trunc((count) / routes.files.length * 100) + " %";
+            }
+            catch (error) {
                 return;
-              }
+            }
             count += 1;
         }
-        
-        
     }
 }
 
-class Map extends React.Component{
-    render(){
+class Map extends React.Component {
+    render() {
         return <LoadRoute></LoadRoute>
     }
 }
