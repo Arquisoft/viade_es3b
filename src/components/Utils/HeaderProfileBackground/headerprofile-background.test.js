643bc918b716274c5a08d230e896e105e9fec29c
ALfonso Lozana <uo264354@uniovi.es>
2020-04-24 20:32:09 +0200

    Solucionando errores de travis

--- src/components/Utils/HeaderProfileBackground/headerprofile-background.test.js
+++ src/components/Utils/HeaderProfileBackground/headerprofile-background.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
-import HeaderProfileBackground from './presentation-background.component';
+import HeaderProfileBackground from './headerprofile-background.component';
 
 afterAll(cleanup);
 
