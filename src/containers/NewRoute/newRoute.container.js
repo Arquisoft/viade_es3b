ea04733836f4fe4fdf6835124d644ed33266e126
ALfonso Lozana <uo264354@uniovi.es>
2020-04-25 12:28:41 +0200

    Añadiendo lineas

--- src/containers/NewRoute/newRoute.container.js
+++ src/containers/NewRoute/newRoute.container.js
@@ -13,14 +13,17 @@ export default class NewRoute extends React.Component {
   constructor() {
     super();
     this.state = {
-      markers: [[19.4100819, -99.1630388]]
+      markers: [[19.4100819, -99.1630388]],
+      polyline : [[19.4100819, -99.1630388]]
     };
   }
   
   addMarker = (e) => {
-    const {markers} = this.state
+    const {markers,polyline} = this.state
     markers.push(e.latlng)
-    this.setState({markers})
+    polyline.push(e.latlng)
+    console.log(polyline);
+    this.setState({markers,polyline})
   }
 
   render() {
@@ -35,10 +38,13 @@ export default class NewRoute extends React.Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {this.state.markers.map((position, idx) => 
-          <Marker key={`marker-${idx}`} position={position}>
-        </Marker>
+        {this.state.polyline.map(() => 
+           <Polyline  color={'blue'} positions={this.state.polyline}></Polyline>
         )}
+        {this.state.markers.map((position, idx) => 
+          <Marker key={`marker-${idx}`} position={position}></Marker>
+        )}
+
       </Map>
     );
   }
