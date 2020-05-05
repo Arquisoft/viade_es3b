12d356572aef2f98319eec6a43a04278e7f8a85c
JorgeIturrioz <jorgeiturrioz@gmail.com>
2020-05-05 17:29:41 +0200

    Eliminado código innecesario que mostraba banderas

--- src/components/Utils/LanguageDropdown/language-dropdown.component.js
+++ src/components/Utils/LanguageDropdown/language-dropdown.component.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import { Dropdown } from '@util-components';
 import { toast } from 'react-toastify';
 
-const languages = {
-  en: {
-    id: 'en',
-    icon: 'us'
-  },
-  es: {
-    id: 'es',
-    icon: 'es'
-  },
-  'en-US': {
-    id: 'en-US',
-    icon: 'us'
-  }
-};
-
 type Props = {
   i18n: Object,
   t: Function
@@ -41,7 +26,6 @@ class LanguageDropdown extends Component<Props> {
 
   render() {
     const { t } = this.props;
-    const { language } = this.state;
     const profileOpts = [
       {
         label: t('navBar.languages.en'),
